dab1e084e7bdd2eadb9d72399e4d1e4c39efa71a
LeeviMaatta <lepa2505@gmail.com>
2025-01-07 13:49:41 +0200

    Viikkotehtava1 valmis

--- Viikkotehtava1/main.cpp
+++ Viikkotehtava1/main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
+int game(int);
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int maxNum = 20;
+    int guessCount = 0;
+
+    guessCount = game(maxNum);
+
+    cout << "Arvasit " << guessCount << " kertaa" << endl;
+
     return 0;
+}
+
+int game(int maxNum)
+{
+    int guessCount = 0;
+
+    srand(time(0));
+    cout << "Arvaa luku valilta 0 - 20: ";
+    int randomNum = rand() % maxNum;
+
+    while (1) {
+        int guessedNum = 0;
+
+        cin >> guessedNum;
+        guessCount++;
+
+        if (guessedNum == randomNum) {
+            cout << "Arvasit oikein!" << endl;
+            break;
+        }
+        else if (guessedNum != randomNum) {
+            if (guessedNum > randomNum) {
+                cout << "Arvasit liian ison numeron, arvaa uudelleen: ";
+            }
+            else if (guessedNum < randomNum) {
+                cout << "Arvasit liian pienen numeron, arvaa uudelleen: ";
+            }
+        }
+    }
+    return guessCount;
 }
